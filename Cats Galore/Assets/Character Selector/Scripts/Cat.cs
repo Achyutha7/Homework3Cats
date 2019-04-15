abdbd9efa67a36c9e51c59ed467f1abf1e0a64a2
Achyutha <49208363+Achyutha7@users.noreply.github.com>
2019-04-15 12:39:16 -0500

    Added unity project

---
+++ Cats Galore/Assets/Character Selector/Scripts/Cat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public Material[] furColor;
+    public Material[] eyeColor;
+    public GameObject body;
+    public GameObject rightEye;
+    public GameObject leftEye;
+
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int colorIndex = (int)(Random.value * 3);
+        RenderCube(body, colorIndex, furColor);
+
+        colorIndex = (int)(Random.value * 3);
+        RenderCube(rightEye, colorIndex, eyeColor);
+        RenderCube(leftEye, colorIndex, eyeColor);
+    }
+
+    // Update is called once per frame
+    private void RenderCube(GameObject obj, int colorIndex, Material[] color)
+    {
+        rend = obj.GetComponent<Renderer>();
+        rend.enabled = true;
+
+        rend.sharedMaterial = color[colorIndex];
+    }
+}
---
+++ Cats Galore/Assets/Character Selector/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    //UI Text element created to display the fight text
+    public Text fightText;
+    //GameObject myCat is instantiated
+    public GameObject myCat; 
+    //Create a list of gameobjects of cats
+    public List<GameObject> Cats = new List<GameObject>();
+    
+    
+    public void Spawn(Vector3 position)
+    {
+        Instantiate(myCat).transform.position = position;
+        fightText.text = "";
+    }
+    int listSize = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //Make Z position the Z position of prefab object
+            Vector3 adjustZ = new Vector3(worldPoint.x, worldPoint.y, myCat.transform.position.z);
+
+            Spawn(adjustZ);
+            Cats.Add(myCat);
+            myCat = Cats[0];
+            
+
+            foreach (GameObject mycat in Cats)
+            {
+                
+                int listSize = Cats.Count;
+                listSize++;
+            }
+            if (listSize > 8)
+            {
+                fightText.text = "Cats Fight!";
+            }
+        }
+    }
+}
